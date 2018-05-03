64e02f767b5dc2b455540f587c9cea2bfa11eb6c
wl <1258481281@qq.com>
2018-05-03 15:02:12 +0800

    Adding config directory

---
+++ config/query_config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2017/3/24
+
+@author: will4906
+"""
+from entity.query_item import SipoItem, DateSelect, And, ItemGroup, Or, Not
+
+QUERY_LIST = [
+    SipoItem(key_word='电动汽车', instructions='新能源汽车'),
+]
+
+
